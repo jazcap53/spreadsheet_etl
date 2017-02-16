5b911cb528da5cce7ac152134ddb9b20566383db
Andrew Jarcho <andrew.jarcho@gmail.com>
2017-02-16 11:26:22 -0500

    Begin work on the db
    Your branch is ahead of 'origin/master' by 1 commit.
    Changes to be committed:
        new file:   sql/create_tables.sql

---
+++ sql/create_tables.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS sl_night CASCADE;
+
+CREATE TABLE sl_night (
+    night_id SERIAL UNIQUE,
+    start_date date NOT NULL,
+    start_time time NOT NULL
+);
+
+
+DROP TABLE IF EXISTS sl_nap;
+
+CREATE TABLE sl_nap (
+    start_time time NOT NULL UNIQUE, -- serves as a primary key
+    duration interval NOT NULL,
+    night_id INTEGER,
+    PRIMARY KEY (start_time),
+    FOREIGN KEY (night_id) REFERENCES sl_night (night_id)
+);
