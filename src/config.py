eba52f64016ea35c34550cb65343b5f5489154be
Andrew Jarcho <andrew.jarcho@gmail.com>
2017-02-22 10:55:45 -0500

    Create skeleton for the LOAD step of etl.
    The newly added files are minimal placeholders. They have not been
    connected to the EXTRACT functionality, which is well developed.
    
    On branch master
    Changes to be committed:
        new file:   src/config.py
        new file:   src/database.ini
        new file:   src/load.py

---
+++ src/config.py
@@ -0,0 +1,23 @@
+# file: src/config.py
+# andrew jarcho
+# 2017-02-22
+
+# python 2.7, 3.5
+
+
+from configparser import ConfigParser
+
+
+def config(filename='src/database.ini', section='postgresql'):
+    """ Read the configuration file for the db connection. """
+    parser = ConfigParser()
+    parser.read(filename)
+    db = {}
+    # TODO: change from LBYL to EAFP?
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+    else:
+        raise Exception('Section {} not found in the {} file'.format(section, filename))
+    return db
---
+++ src/load.py
@@ -0,0 +1,36 @@
+# file: src/load.py
+# andrew jarcho
+# 2017-02-20
+
+# python 2.7, 3.5
+
+
+from __future__ import print_function
+
+import psycopg2
+from config import config
+
+from src.container_objs import Event, Day, Week
+
+def connect():
+    """
+    Connect to the PostgreSQL database server
+    and execute a simple query
+    """
+    conn = None
+    try:
+        params = config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        cur.execute('SELECT version()')
+        db_version = cur.fetchone()
+        print(db_version)
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+if __name__ == '__main__':
+    connect()
