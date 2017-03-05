c812d5d62c549c988e453d63ac428dd15175dbe1
Andrew Jarcho <andrew.jarcho@gmail.com>
2017-03-05 11:29:14 -0500

    Simplify src/transform/get_input.py
    On branch master
    Changes to be committed:
        modified:   src/transform/get_input.py

--- src/transform/get_input.py
+++ src/transform/get_input.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3.5
 
 
-# file: src/get_input.py
+# file: src/transform/get_input.py
 # andrew jarcho
 # 2017-02-22
 
 
-line = input().rstrip()
 while True:
-    print(line)
     try:
         line = input().rstrip()
+        print(line)
     except EOFError:
         break
