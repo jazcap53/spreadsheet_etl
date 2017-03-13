16f90f694bad8853cd6b29797e47384a52b8f90c
Andrew Jarcho <andrew.jarcho@gmail.com>
2017-03-12 21:58:18 -0400

    Continue work on module do_transform.py, whose output will serve as input for the load phase
    Intermediate commit. do_transform.py runs without error, and outputs NIGHTs, but does not yet
    output full NAP data.
    
    On branch inside_work_on_transform
    Changes to be committed:
        modified:   src/transform/do_transform.py

--- src/transform/do_transform.py
+++ src/transform/do_transform.py
@@ -6,32 +6,60 @@
 # 2017-03-12
 
 
-def process_curr(cur_l):
+def process_curr():
     # print('in process_cur, cur_l is {}'.format(cur_l))
     out_val = None
-    try:
-        if cur_l == '':
-            pass
-        elif cur_l[0] == 'W':
-            pass
-        elif cur_l[0] == '=':
-            pass
-        elif cur_l[0] == ' ':
-            out_val = cur_l
-        elif cur_l[: 9] == 'action: b':
-            out_val = cur_l
-        elif cur_l[: 9] == 'action: s':
-            out_val = cur_l
-        elif cur_l[: 9] == 'action: w':
-            out_val = cur_l
-    except IndexError:
-        print('BAD VALUE {} in input'.format(cur_l))
-    else:
-        if out_val is not None:
-            print(out_val)
+    last_date = ''
+    last_sleep_time = ''
+    multiplier = 0
+    def inner_process_curr(cur_l):
+        nonlocal out_val, last_date, last_sleep_time, multiplier
+        try:
+            if cur_l == '':
+                pass
+            elif cur_l[0] == 'W':
+                pass
+            elif cur_l[0] == '=':
+                pass
+            elif cur_l[0] == ' ':  # a date in the format '    yyyy-mm-dd'
+                last_date = cur_l[4: ]
+            elif cur_l[: 9] == 'action: b':
+                # next 5 lines: needed because 'time: ' part of cur_l may
+                # be 'h:mm' or 'hh:mm'
+                end_pos = cur_l.rfind(', hours: ')
+                if end_pos != -1:
+                    last_sleep_time = cur_l[17: end_pos]
+                else:
+                    last_sleep_time = cur_l[17: ]
+                out_val = 'NIGHT, {}, {}'.format(last_date, last_sleep_time)
+            elif cur_l[: 9] == 'action: s':
+                # last_sleep_time = cur_l[17: ]
+                # next 5 lines: needed because 'time: ' part of cur_l may
+                # be 'h:mm' or 'hh:mm'
+                end_pos = cur_l.rfind(', hours: ')
+                if end_pos != -1:
+                    last_sleep_time = cur_l[17: end_pos]
+                else:
+                    last_sleep_time = cur_l[17: ]
+            elif cur_l[: 9] == 'action: w':
+                w_action_time = cur_l[17: ]
+                if w_action_time < last_sleep_time:
+                    multiplier = 1
+                else:
+                    multiplier = 0
+                # TODO: stuff goes here to calculate interval output
+                out_val = 'NAP, something, something<, something>'
+        except IndexError:
+            print('BAD VALUE {} in input'.format(cur_l))
+        else:
+            if out_val is not None:
+                print(out_val)
+            out_val = None
+    return inner_process_curr
 
 
 def read_each_line():  # from outp.stdout, which has been set to PIPE
+    line_processor = process_curr()
     while True:
         try:
             curr_line = input()
@@ -39,7 +67,7 @@ def read_each_line():  # from outp.stdout, which has been set to PIPE
         #     try:
         #         next_line = input()
         #         process_curr(curr_line, next_line)
-            process_curr(curr_line)
+            line_processor(curr_line)
         #         curr_line = next_line
         except EOFError:
             break
