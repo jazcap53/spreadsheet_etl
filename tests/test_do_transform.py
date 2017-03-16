0e05261a16066030f31ac429dcad4e1446bdfa4b
Andrew Jarcho <andrew.jarcho@gmail.com>
2017-03-16 19:33:05 -0400

    Set up test_do_transform.py as a temporary dummy test
    On branch add_pytests
    Changes to be committed:
        modified:   tests/test_do_transform.py

--- tests/test_do_transform.py
+++ tests/test_do_transform.py
@@ -2,18 +2,22 @@
 # andrew jarcho
 # 2017-03-15
 
-# nosetests 1.3.7
+# pytest 3.0.7
 
-import unittest
-from unittest import TestCase
+
+import pytest
 
 from src.transform.do_transform import process_curr
 
 
-class TestDoTransform(TestCase):
+@pytest.fixture
+def inner_process_curr():
+    return process_curr()
 
-    def setUp(self):
-        self.p_c = process_curr()
 
-    def test_nada(self):
-        self.assertTrue(True)
+def test_nada(inner_process_curr):
+    cur_l = '    2017-01-12'
+    inner_process_curr(cur_l)
+    assert True
+
+
