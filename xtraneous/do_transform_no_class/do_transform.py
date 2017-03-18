a723fe7d92c11828a33ed3f2dd5b8cd7f667a199
Andrew Jarcho <andrew.jarcho@gmail.com>
2017-03-18 16:58:45 -0400

    Keep working on the non-class version of do_transform.py
    Am having success with this; it may wind up being the version I choose.
    It's a question of picking either a class that will end up being
    a singleton, or a non-class implementation that uses a closure
    but is a bit overlong (readability may suffer).
    
    On branch work_on_old_do_transform
    Changes to be committed:
        new file:   xtraneous/do_transform_no_class/do_transform.py
        new file:   xtraneous/do_transform_no_class/tests/test_do_transform.py

---
+++ xtraneous/do_transform_no_class/do_transform.py
@@ -0,0 +1,123 @@
+#!/usr/bin/python3.5
+
+
+# file: src/transform/do_transform.py
+# andrew jarcho
+# 2017-03-12
+
+
+"""
+Read lines from stdin, transform to a db-friendly format, and write to stdout.
+
+process_curr() returns a closure: inner_process_curr. inner_process_curr()
+takes a string as an argument, and prints a corresponding command to stdout
+that may be used to load data from the argument string into the db.
+"""
+
+def process_curr():
+    """
+    Called by: read_each_line()
+    Returns: inner_process_curr()
+    """
+    out_val = None
+    last_date = ''
+    last_sleep_time = ''
+    multiplier = 0
+
+
+    def get_wake_or_last_sleep(cur_l):
+        """
+        Called by: inner_process_curr().
+        Returns: the 'time' part of cur_l, which it receives in 'h:mm' or
+                 'hh:mm' format.
+        """
+        end_pos = cur_l.rfind(', hours: ')
+        out_time = cur_l[17: ] if end_pos == -1 else cur_l[17: end_pos]
+        if len(out_time) == 4:
+            out_time = '0' + out_time
+        return out_time
+
+
+    def get_duration(w_time, s_time):
+        """
+        Called by: inner_process_curr()
+        Returns: the difference between the wake and sleep times,
+                 expressed as a string in decimal format, e.g.,
+                 04.25 for 4 1/4 hours.
+        """
+        w_time_list = list(map(int, w_time.split(':')))
+        s_time_list = list(map(int, s_time.split(':')))
+        if w_time_list[1] < s_time_list[1]:  # wake minit < sleep minit
+            w_time_list[1] += 60
+            w_time_list[0] -= 1
+        if w_time_list[0] < s_time_list[0]:  # wake hour < sleep hour
+            w_time_list[0] += 24
+        dur_list = [(w_time_list[x] - s_time_list[x]) for x in range(len(w_time_list))]
+        duration = str(dur_list[0])
+        if len(duration) == 1:  # change hour from '1' to '01', e.g.
+            duration = '0' + duration
+        if dur_list[1] == 15:
+            duration += '.25'
+        elif dur_list[1] == 30:
+            duration += '.50'
+        elif dur_list[1] == 45:
+            duration += '.75'
+        elif dur_list[1] == 0:
+            duration += '.00'
+        return duration
+
+
+    def inner_process_curr(cur_l):
+        """
+        Called by: inner_process()
+        Returns: None
+        Prints argument to stdout as a string in format:
+               'NIGHT, date, time'  or
+               'NAP, time, duration'
+        """
+        nonlocal out_val, last_date, last_sleep_time, multiplier
+        nonlocal get_wake_or_last_sleep, get_duration
+        try:
+            if cur_l == '':
+                pass
+            elif cur_l[0] == 'W':  # 'Week of ...'
+                pass
+            elif cur_l[0] == '=':  # '========...'
+                pass
+            elif cur_l[0] == ' ':  # a date in the format '    yyyy-mm-dd'
+                last_date = cur_l[4: ]
+            elif cur_l[: 9] == 'action: b':
+                last_sleep_time = get_wake_or_last_sleep(cur_l)
+                out_val = 'NIGHT, {}, {}'.format(last_date, last_sleep_time)
+            elif cur_l[: 9] == 'action: s':
+                last_sleep_time = get_wake_or_last_sleep(cur_l)
+            elif cur_l[: 9] == 'action: w':
+                wake_time = get_wake_or_last_sleep(cur_l)
+                duration = get_duration(wake_time, last_sleep_time)
+                out_val = 'NAP, {}, {}'.format(wake_time, duration)
+        except IndexError:
+            print('BAD VALUE {} in input'.format(cur_l))
+        else:
+            if out_val is not None:
+                print(out_val)
+            out_val = None
+
+    return inner_process_curr
+
+
+def read_each_line():
+    """
+    Called by: __main__()
+    Read from the 'extract' phase output; write to stdout.
+    """
+    line_processor = process_curr()
+    while True:
+        try:
+            curr_line = input()
+            line_processor(curr_line)
+        except EOFError:
+            break
+
+
+if __name__ == '__main__':
+    read_each_line()
---
+++ xtraneous/do_transform_no_class/tests/test_do_transform.py
@@ -0,0 +1,71 @@
+# file: tests/test_do_transform.py
+# andrew jarcho
+# 2017-03-15
+
+# pytest 3.0.7
+
+
+import pytest
+import sys
+from contextlib import contextmanager
+from io import StringIO
+
+from spreadsheet_etl.xtraneous.do_transform_no_class.do_transform import process_curr
+
+
+@contextmanager
+def captured_output():
+    new_out, new_err = StringIO(), StringIO()
+    old_out, old_err = sys.stdout, sys.stderr
+    try:
+        sys.stdout, sys.stderr = new_out, new_err
+        yield sys.stdout, sys.stderr
+    finally:
+        sys.stdout, sys.stderr = old_out, old_err
+
+
+@pytest.fixture
+def inner_process_curr():
+    return process_curr()
+
+
+def test_call_to_inner_process_curr_with_date_line_stores_last_date(inner_process_curr):
+    cur_l = '    2017-01-12'
+    inner_process_curr(cur_l)
+    assert len(inner_process_curr.__code__.co_freevars) == 6
+    assert inner_process_curr.__code__.co_freevars[2] == 'last_date'
+    assert inner_process_curr.__closure__[2].cell_contents == '2017-01-12'
+
+
+def test_call_to_inner_process_curr_with_action_b_stores_last_sleep_time(inner_process_curr):
+    cur_l = 'action: b, time: 0:00, hours: 9.00'
+    inner_process_curr(cur_l)
+    assert inner_process_curr.__closure__[3].cell_contents == '00:00'  # __closure__[3] is last_sleep_time
+
+
+def test_call_to_inner_process_curr_with_date_and_action_b_outputs_NIGHT(inner_process_curr):
+    cur_l = '    2017-01-12'
+    inner_process_curr(cur_l)
+    cur_l = 'action: b, time: 0:00, hours: 9.00'
+    with captured_output() as (out, err):
+        inner_process_curr(cur_l)
+    output = out.getvalue().strip()
+    assert output == 'NIGHT, 2017-01-12, 00:00'
+
+
+def test_call_to_inner_process_curr_with_action_s_stores_last_sleep_time(inner_process_curr):
+    cur_l = 'action: s, time: 13:15'
+    inner_process_curr(cur_l)
+    assert inner_process_curr.__closure__[3].cell_contents == '13:15'
+
+
+def test_call_to_inner_process_curr_with_date_and_action_s_action_w_outputs_NAP(inner_process_curr):
+    cur_l = '    2016-09-12'
+    inner_process_curr(cur_l)
+    cur_l = 'action: s, time: 03:00'
+    inner_process_curr(cur_l)
+    cur_l = 'action: w, time: 04:15, hours: 01.15'
+    with captured_output() as (out, err):
+        inner_process_curr(cur_l)
+    output = out.getvalue().strip()
+    assert output == 'NAP, 04:15, 01.25'
