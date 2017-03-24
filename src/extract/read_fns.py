2cea1df33401b9aeb17e52ca8c349422bd6823e6
Andrew Jarcho <andrew.jarcho@gmail.com>
2017-03-24 07:28:35 -0400

    Finish cleanup of read_lines()
    On branch refactor_read_fns
    Changes to be committed:
        modified:   src/extract/read_fns.py

--- src/extract/read_fns.py
+++ src/extract/read_fns.py
@@ -5,6 +5,37 @@
 # python: 2.7, 3.5
 
 
+"""
+The input is structured in lines as:
+
+    Sunday Date   Sun   Mon  Tue  Wed  Thu  Fri  Sat
+                                   x    x    x    x
+                                   x         x    x
+                                   x              x
+                                                  x
+
+
+    Sunday Date    x     x    x    x    x    x    x
+                   x          x    x         x    x
+                              x    x         x
+                              x              x
+                              x
+
+    where each 'x' is a data item (event) we care about.
+
+The output is structured into Events, Days, and Weeks.
+The Events from each Day are grouped together. A Week
+groups 7 consecutive Days, beginning with a Sunday.
+
+Once the input file has been opened, function read_lines()
+controls the data processing: all other functions are
+called from read_lines().
+
+read_lines() returns a list of Weeks to the client.
+"""
+
+
+
 from __future__ import print_function
 
 import sys
@@ -22,15 +53,14 @@ def open_file(file_read_wrapper):
     return file_read_wrapper.open()
 
 
-# TODO: make this function easier to read
-# TODO: get rid of do_append_week? sunday_date? new_week?
 def read_lines(infile, weeks, sunday_date=None, do_append_week=False,
                new_week=None):
     """
-    Read and discard lines until a Sunday marker is found.
-    Then create a Week as a list of 7 Days, and insert data into it
-    until a blank line is read. On reading a blank line, append the
-    Week just read to the weeks list.
+    Ignore lines that appear before the first Sunday is seen.
+    Thereafter, collect data until a blank line is seen.
+    Append that data to the output, and resume ignoring lines
+    until the next Sunday is seen, or EOF.
+
     Returns: the weeks list.
     Called by: client code
     """
@@ -38,23 +68,23 @@ def read_lines(infile, weeks, sunday_date=None, do_append_week=False,
     wks_pls = WeeksPlus(weeks, sunday_date, do_append_week, new_week)
     for line in infile:
         line = line.strip().split(',')
-        if not any(line):      # we had a blank row in the spreadsheet
-            if not wks_pls.sunday_date:
+        if not any(line):
+            if not wks_pls.sunday_date:     # we're looking for a Sunday
                 continue
             else:
                 wks_pls = _append_week(wks_pls)
-        elif not wks_pls.sunday_date:  # we haven't seen a Sunday yet this week
+        elif not wks_pls.sunday_date:
             date_match = _check_for_date(line[0])
-            if date_match:
-                wks_pls = wks_pls._replace(sunday_date=_match_to_date_obj(date_match))
+            if date_match:                  # we've found a Sunday
+                wks_pls = wks_pls._replace(sunday_date = _match_to_date_obj(date_match))
                 day_list = [Day(wks_pls.sunday_date + datetime.timedelta(days=x), [])
-                        for x in range(7)]
-                wks_pls = wks_pls._replace(new_week=Week(*day_list))
+                        for x in range(7)]  # collect 7 Days into a day_list
+                wks_pls = wks_pls._replace(new_week = Week(*day_list))  # create a Week
             else:
                 continue
         if any(line[1: ]):
             got_events = _get_events(line[1: ], wks_pls.new_week)
-            wks_pls = wks_pls._replace(do_append_week=got_events[0], new_week=got_events[1])
+            wks_pls = wks_pls._replace(do_append_week = got_events[0], new_week = got_events[1])
     # save any remaining unstored data
     wks_pls = _append_week(wks_pls)
     return wks_pls.weeks
@@ -63,8 +93,8 @@ def read_lines(infile, weeks, sunday_date=None, do_append_week=False,
 def _append_week(wks_pls):
     """
     If we have a new Week object, append it to the weeks element of
-    weeks_plus.
-    Returns: If we have a new Week object, an updated weeks_plus
+    wks_pls.
+    Returns: If we have a new Week object, an updated wks_pls
              Else, the function's argument
     Called by: read_lines()
     """
@@ -72,21 +102,21 @@ def _append_week(wks_pls):
         my_new_week = wks_pls.new_week
         wks_pls.weeks.append(my_new_week)
         wks_pls = wks_pls._replace(sunday_date=None, do_append_week=False, new_week=None)
-        # return [weeks, None, False, None]
     return wks_pls
 
 
 def _check_for_date(s):
     """
+    Is the first element of s a date?
     Called by: read_lines()
     """
-    # TODO: replace with more sophisticated test?
     m = re.match(r'(\d{1,2})/(\d{1,2})/(\d{4})', s)
     return m if m else None
 
 
 def _match_to_date_obj(m):
     """
+    Convert a successful regex match to a datetime.date
     Called by: read_lines()
     """
     # group(3) is the year, group(1) is the month, group(2) is the day
