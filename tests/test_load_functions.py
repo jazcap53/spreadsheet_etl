9828e1dfbb1b64d6c36774bc0cd88b4742b0d7e5
Andrew Jarcho <andrew.jarcho@gmail.com>
2017-08-13 19:53:42 -0400

    Improve sql in db integration test
    On branch logging_testable_load
    Changes to be committed:
        modified:   tests/test_load_functions.py

--- tests/test_load_functions.py
+++ tests/test_load_functions.py
@@ -1,5 +1,6 @@
 import pytest
-from sqlalchemy import create_engine, select, func, table
+from datetime import datetime
+from sqlalchemy import create_engine, text
 
 
 @pytest.fixture(scope="module")
@@ -10,15 +11,19 @@ def my_setup():
 
 
 def test_inserting_a_night_adds_one_to_night_count(my_setup):
+    date_time_now = datetime.now()
+    date_today = date_time_now.date().isoformat()
+    time_now = date_time_now.time().isoformat()
+    last_colon_at = time_now.rfind(':')
+    time_now = time_now[:last_colon_at] + ':00'
     engine = my_setup
     connection = engine.connect()
-    # result = connection.execute("select([func.count()]).select_from(table)")
     result = connection.execute("select count (*) from slt_night")
     orig_ct = result.fetchone()[0]
-    connection.execute("insert into slt_night (start_date, start_time)"
-                       " values('2017-08-05', '07:15')")
+    sql = text("INSERT INTO slt_night (start_date, start_time)"
+               " values (:date_today, :time_now)")
+    data = {'date_today': date_today, 'time_now': time_now}
+    connection.execute(sql, data)
     result = connection.execute("select count (*) from slt_night")
     new_ct = result.fetchone()[0]
     assert orig_ct + 1 == new_ct
-    # print('orig_ct is {}'.format(orig_ct))
-
