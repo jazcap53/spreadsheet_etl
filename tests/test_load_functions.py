d4667e532c5e97b4fe62db736e41deb3e0777611
Andrew Jarcho <andrew.jarcho@gmail.com>
2017-08-12 20:25:47 -0400

    Create test database and associated files; revise db integration test to work with test db
    On branch logging_testable_load
    Changes to be committed:
        new file:   db_test/config.py
        new file:   db_test/create_procedures.sql
        new file:   db_test/create_tables.sql
        new file:   db_test/grant_privileges.sql
        modified:   tests/test_load_functions.py

--- tests/test_load_functions.py
+++ tests/test_load_functions.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine, select, func, table
 
 @pytest.fixture(scope="module")
 def my_setup():
-    url = 'postgresql://jazcap53:test@localhost/sleep'
+    url = 'postgresql://jazcap53:test@localhost/sleep_test'
     engine = create_engine(url)
     return engine
 
@@ -13,11 +13,11 @@ def test_inserting_a_night_adds_one_to_night_count(my_setup):
     engine = my_setup
     connection = engine.connect()
     # result = connection.execute("select([func.count()]).select_from(table)")
-    result = connection.execute("select count (*) from sl_night")
+    result = connection.execute("select count (*) from slt_night")
     orig_ct = result.fetchone()[0]
-    connection.execute("insert into sl_night (start_date, start_time)"
+    connection.execute("insert into slt_night (start_date, start_time)"
                        " values('2017-08-05', '07:15')")
-    result = connection.execute("select count (*) from sl_night")
+    result = connection.execute("select count (*) from slt_night")
     new_ct = result.fetchone()[0]
     assert orig_ct + 1 == new_ct
     # print('orig_ct is {}'.format(orig_ct))
