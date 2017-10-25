908b8811ef82705aedf174b00619320a67ea17e1
Andrew Jarcho <andrew.jarcho@gmail.com>
2017-10-25 06:47:38 -0400

    Continue to refactor for clarity
    Rename variables and functions, update docstrings and comments
    
    On branch simplify_read_fns
    Changes to be committed:
        modified:   src/extract/read_fns.py

--- src/extract/read_fns.py
+++ src/extract/read_fns.py
@@ -60,7 +60,7 @@ def read_lines(infile):
         Ignore lines until a Sunday is seen.
         Collect one week of data (until a blank line is seen).
         Append that week of data to lines_buffer[].
-        Call _manage_buffer() to write good data, discard bad data,
+        Call _handle_end_of_week() to write good data, discard bad data,
             and empty the buffer.
     Until:
         EOF.
@@ -73,12 +73,12 @@ def read_lines(infile):
     new_week = None
     lines_buffer = []
     for line in infile:
-        line = line.strip().split(',')[:22]
-        date_match_found = _check_for_date(line[0])
-        if not in_week and not date_match_found:
+        line_as_list = line.strip().split(',')[:22]
+        re_date_match_found = _check_for_date(line_as_list[0])
+        if not in_week and not re_date_match_found:
             continue
-        elif not in_week and date_match_found:  # we *just* found a date
-            sunday_date = _match_to_date_obj(date_match_found)
+        elif not in_week and re_date_match_found:  # we *just* found a date
+            sunday_date = _re_match_to_date_obj(re_date_match_found)
             # create a day_list of 7 (empty) Days
             day_list = [Day(sunday_date +
                             datetime.timedelta(days=x), [])
@@ -86,16 +86,16 @@ def read_lines(infile):
             new_week = Week(*day_list)
             in_week = True
         if in_week:
-            if not any(line):  # a blank line: our week has ended
-                _manage_buffer(new_week, lines_buffer)
+            if not any(line_as_list):  # a blank line: our week has ended
+                _handle_end_of_week(new_week, lines_buffer)
                 in_week = False
                 sunday_date = None
                 # assert lines_buffer == []  # TODO: remove this
             else:  # add the line's events to the present week
-                have_events = _get_events(line[1:], new_week)
+                have_events = _get_events(line_as_list[1:], new_week)
     # manage any remaining unstored data
     if sunday_date and have_events and new_week:
-        _manage_buffer(new_week, lines_buffer)
+        _handle_end_of_week(new_week, lines_buffer)
 
 
 def _check_for_date(field):
@@ -107,9 +107,9 @@ def _check_for_date(field):
     return m if m else None
 
 
-def _match_to_date_obj(m):
+def _re_match_to_date_obj(m):
     """
-    Convert a successful regex match to a datetime.date
+    Convert a successful regex match to a datetime.date object
     Called by: read_lines()
     """
     # group(3) is the year, group(1) is the month, group(2) is the day
@@ -118,15 +118,17 @@ def _match_to_date_obj(m):
     return dt_obj
 
 
-def _get_events(line, new_week):
+def _get_events(shorter_line, new_week):
     """
-    Add each valid event in line to new_week.
+    Add each valid event in shorter_line to new_week.
+    :param shorter_line: line_as_list, from read_lines(),
+        without its first field
     Called by: read_lines()
     """
     find_events = False
     for ix in range(7):
         # a segment is a list of 3 consecutive fields from the .csv file
-        segment = line[3*ix: 3*ix + 3]
+        segment = shorter_line[3 * ix: 3 * ix + 3]
         if validate_segment(segment):
             an_event = Event(*segment)
         elif segment == ['', '', '']:
@@ -143,18 +145,21 @@ def _get_events(line, new_week):
 
 
 # TODO: write docstring, comments
-def _manage_buffer(wk, buffer):
+def _handle_end_of_week(wk, buffer):
     """
 
 
-    :param wk: a Week object, holding Days some of which might have
-               data missing
-    :param buffer:
-    :return:
+    :param wk: a Week object, holding Days some of which might be
+               missing data (incomplete).
+    :param buffer: a list, possibly empty, of header strings and event
+                   strings.
+    :return: None
     """
-    wk_header = '\nWeek of Sunday, {}:\n'.format(wk[0].dt_date)
-    underscores = '=' * (len(wk_header) - 2)
-    buffer.append(wk_header + underscores)
+    if not buffer:
+        read_logger.debug('Empty buffer in _handle_end_of_week()')  # TODO: remove this line
+    wk_header = '\nWeek of Sunday, {}:'.format(wk[0].dt_date)
+    wk_header += '\n' + '=' * (len(wk_header) - 2)
+    buffer.append(wk_header)
     for day in wk:
         dy_header = '    {}'.format(day.dt_date)  # four leading spaces
         buffer.append(dy_header)
@@ -173,32 +178,36 @@ def _handle_end_of_day(buffer, action_b_event):
     """
     Write complete Day's (only) from buffer to stdout.
 
-    :param buffer: may contain header lines and/or 'action' lines
+    :param buffer: a list of strings. May contain header lines and/or
+                   Event lines. Event lines start with 'action'.
     :param action_b_event: is the last Event of some Day.
-        action_b_event will have an hours field <=> we have complete
-            data for that Day.
+                           action_b_event will have an hours field <=>
+                           we have complete data for that Day.
 
-    if we have complete data:
+    if we have complete data for this Day:
         output the entire buffer
         clear the buffer
     else:
         do not output anything
-        remove 'action' lines from the buffer
+        remove Event lines from the buffer
         leave any header lines
 
-    Called by: _manage_buffer()
+    Called by: _handle_end_of_week()
     """
     if action_b_event.hours:  # we have a complete Day
-        for line in buffer:  # action_b_event is not yet in buffer (!!!)
+        for line in buffer:  # note: action_b_event is not yet in buffer
             print(line)
         buffer.clear()
-    else:  # Day is *incomplete*: pop 'action' lines only
+    else:  # Day is incomplete: pop Event lines only, starting from end
+        read_logger.debug('Incomplete Day')  # TODO: remove this line
         for buf_ix in range(len(buffer) - 1, -1, -1):
             this_line = buffer[buf_ix]
-            # if we find a 3-element 'b' event
+            # if we reach a 3-element 'b' event
+            # i.e., if the last Day handled was incomplete
             if this_line != '\n' and this_line[8] == 'b' and \
                             len(this_line) > 21:
+                read_logger.debug('reached 3-element b-event {}'.format(this_line))  # TODO: remove this line
                 buffer.pop(buf_ix)  # pop one last time, then stop
                 break
-            elif this_line[:6] == 'action':  # pop only 'action' lines:
+            elif this_line[:6] == 'action':  # pop only Event lines:
                 buffer.pop(buf_ix)           # leave headers in buffer
