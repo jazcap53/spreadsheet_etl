409861cca2698d524e35b769bc3080ce67425c6c
Andrew Jarcho <andrew.jarcho@gmail.com>
2018-10-11 07:00:57 -0400

    Make second test pass for chart.py
    On branch testr_branch
    Changes to be committed:
        modified:   src/chart/chart.py
        modified:   tests/test_chart.py

--- src/chart/chart.py
+++ src/chart/chart.py
@@ -1,11 +1,21 @@
 class Chart:
-    to_unicode = {-1: '\u2591', 0: '\u0020', 1: '\u2590', 2: '\u258c',
-                  3: '\u2588'}
+    to_unicode = {0: '\u0020', 1: '\u2590', 2: '\u258c',
+                  3: '\u2588', 7: '\u2591'}
 
     def __init__(self):
         pass
-    
+
     @staticmethod
     def get_half_hr(code):
         return Chart.to_unicode[code]
 
+    @staticmethod
+    def make_out_string(line_in):
+        temp = [Chart.get_half_hr(int(i)) for i in line_in.decode()]
+        line_out = ''.join(temp)
+        return line_out
+
+
+Chart.make_out_string(bytearray
+                      ('771333200013332000133320001333200013332000133320',
+                       'utf-8'))
--- tests/test_chart.py
+++ tests/test_chart.py
@@ -23,6 +23,15 @@ def test_get_half_hr_if_3(make_chart):
     assert make_chart.get_half_hr(3) == '\u2588'
 
 
-def test_get_half_hr_if_neg_1(make_chart):
-    assert make_chart.get_half_hr(-1) == '\u2591'
+def test_get_half_hr_if_7(make_chart):
+    assert make_chart.get_half_hr(7) == '\u2591'
+
+
+def test_make_out_string_with_valid_input(make_chart):
+    line_in = bytearray('771333200013332000133320001333200013332000133320', 'utf-8')
+    assert len(line_in) == 48
+    temp2 = make_chart.make_out_string(line_in)
+    line_out = '|' + temp2 + '|\n'
+    assert len(line_out) == 51
+    assert line_out == '|░░▐███▌   ▐███▌   ▐███▌   ▐███▌   ▐███▌   ▐███▌ |\n'
 
