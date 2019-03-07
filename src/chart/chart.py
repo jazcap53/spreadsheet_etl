02fadd2ec5251842a2f97d406c4d4378b26d06c8
Andrew Jarcho <andrew.jarcho@gmail.com>
2019-03-07 01:01:54 -0500

    Write first draft of version that uses 1 char for each 1/4 hour
    On branch to_landscape
    Changes to be committed:
        modified:   src/chart/chart.py
        modified:   tests/test_chart.py

--- src/chart/chart.py
+++ src/chart/chart.py
@@ -6,20 +6,24 @@
 from tests.file_access_wrappers import FileReadAccessWrapper
 import re
 import sys
+import datetime
 
 
 class Chart:
     """
     Create a sleep chart from input data
     """
-    to_glyph = {0: '\u2588',  # FULL BLOCK xx█xx
-                1: '\u258c',  # LEFT HALF BLOCK xx▌xx
-                2: '\u2590',  # RIGHT HALF BLOCK xx▐xx
-                3: '\u0020',  # SPACE xx xx
-                7: '\u2591'}  # LIGHT SHADE xx░xx
+    # to_glyph = {0: '\u2588',  # FULL BLOCK xx█xx
+    #             1: '\u258c',  # LEFT HALF BLOCK xx▌xx
+    #             2: '\u2590',  # RIGHT HALF BLOCK xx▐xx
+    #             3: '\u0020',  # SPACE xx xx
+    #             7: '\u2591'}  # LIGHT SHADE xx░xx
 
-    ASLEEP = 0b0  # the printed color (black ink)
-    AWAKE = 0b1  # the background color (white paper)
+    # ASLEEP = 0b0  # the printed color (black ink)
+    # AWAKE = 0b1  # the background color (white paper)
+
+    ASLEEP = 1  # the printed color (black ink)
+    AWAKE = 0   # the background color (white paper)
 
     def __init__(self, filename):
         self.filename = filename
@@ -28,76 +32,107 @@ class Chart:
         self.prev_line = ''
         self.sleep_state = self.AWAKE
         self.date_re = None
+        # self.today = None
+        self.cur_date_str = ''
+        self.cur_time_str = ''
+        self.cur_date_time_str = ''
+        self.cur_date = None
+        self.cur_time = None
+        self.cur_datetime = None
+        self.days_carried = 0
+        self.day_row = [self.AWAKE] * 24 * 4
+        self.header_seen = False
 
     def get_a_line(self):
         """
 
-        :return:
+        :return: bool
         Called by: open_file()
         """
         self.cur_line = self.infile.readline().rstrip()
-        while self.cur_line and not self.date_re.match(self.cur_line):
-            self.cur_line = self.infile.readline().rstrip()
+        if not self.header_seen:
+            self.skip_header()
         return bool(self.cur_line)
 
+    def skip_header(self):
+        while self.cur_line and not self.date_re.match(self.cur_line):
+            self.cur_line = self.infile.readline().rstrip()
+        self.header_seen = True
+
     @staticmethod
-    def quarter_to_digit(q):
+    def quarter_to_int(q):
         """
-        Return one bit per quarter hour
+        Return one int per quarter hour
         :param q: the state(asleep or awake) during a quarter hour
-        :return: 0-bit for asleep, 1-bit for awake
-        Called by:
+        :return: 1 for asleep, 0 for awake
+        # Called by:
         """
         if q:
-            return Chart.AWAKE
-        return Chart.ASLEEP
+            return Chart.ASLEEP
+        return Chart.AWAKE
 
-    @staticmethod
-    def quarters_to_glyph_code(hi_bit, lo_bit):
-        """
-        Convert 2 1-bit quarters to a 2-bit code
-        :param hi_bit: high order bit
-        :param lo_bit: low order bit
-        :return: a 2-bit code representing a quarter hour
-        Called by:
-        """
-        return hi_bit << 1 | lo_bit
+    # @staticmethod
+    # def quarters_to_glyph_code(hi_bit, lo_bit):
+    #     """
+    #     Convert 2 1-bit quarters to a 2-bit code
+    #     :param hi_bit: high order bit
+    #     :param lo_bit: low order bit
+    #     :return: a 2-bit code representing a quarter hour
+    #     Called by:
+    #     """
+    #     return hi_bit << 1 | lo_bit
 
-    @staticmethod
-    def make_glyph(code):
-        """
-        A glyph names a Unicode Block Element code point
-        :param code: a 2-bit code representing a glyph in dict to_glyph
-        :return: the glyph associated with the 2-bit code
-        Called by: make_out_string()
-        """
-        return Chart.to_glyph[code]
+    # @staticmethod
+    # def make_glyph(code):
+    #     """
+    #     A glyph names a Unicode Block Element code point
+    #     :param code: a 2-bit code representing a glyph in dict to_glyph
+    #     :return: the glyph associated with the 2-bit code
+    #     Called by: make_out_string()
+    #     """
+    #     return Chart.to_glyph[code]
 
-    @staticmethod
-    def make_out_string(line_in):
-        """
-        Convert bytearray with 2-bit values 0,1,2,3,or 7 to a string of glyphs
-        :param line_in:
-        :return:
-        Called by: main()
-        """
-        assert len(line_in)
-        return ''.join([Chart.make_glyph(int(i)) for i in line_in.decode()])
+    # @staticmethod
+    # def make_out_string(line_in):
+    #     """
+    #     Convert bytearray with 2-bit values 0,1,2,3,or 7 to a string of glyphs
+    #     :param line_in:
+    #     :return:
+    #     Called by: main()
+    #     """
+    #     assert len(line_in)
+    #     return ''.join([Chart.make_glyph(int(i)) for i in line_in.decode()])
 
     def open_file(self):
         """
-
-        :return:
+        :return: None
         Called by: main()
         """
         with open(self.filename) as self.infile:
             while self.get_a_line():
-                print(self.cur_line)
+                # print(self.cur_line)
+                print(self.parse_line(), self.cur_datetime)
+
+    def parse_line(self):
+        line_array = self.cur_line.strip().split('|')
+        if len(line_array) < 2:
+            self.cur_datetime = None
+            return
+        self.cur_date_str = line_array[0].strip()
+        self.cur_time_str = line_array[1].strip()
+        self.cur_date_time_str = self.cur_date_str + ((' ' + self.cur_time_str) if
+                                                      self.cur_time_str else '')
+        # print(self.cur_date_time_str)
+        self.cur_datetime = datetime.datetime.strptime(self.cur_date_time_str,
+                                                       ('%Y-%m-%d %H:%M:%S' if
+                                                        self.cur_time_str else
+                                                        '%Y-%m-%d'))
+
+        return line_array
 
     def compile_date_re(self):
         """
-
-        :return:
+        :return: None
         Called by: main()
         """
         self.date_re = re.compile(r' \d{4}-\d{2}-\d{2} \|')
@@ -107,14 +142,14 @@ def main():
     chart = Chart('/home/jazcap53/python_projects/spreadsheet_etl/src/chart/chart_raw_data.txt')
     chart.compile_date_re()
     chart.open_file()
-    print(Chart.make_out_string
-          (bytearray
-           ('771333200013332000133320001333200013332000133320',
-            'utf-8')))
-    print(Chart.make_out_string
-          (bytearray
-           ('000000000000000000000000000000000000000000000000',
-            'utf-8')))
+    # print(Chart.make_out_string
+    #       (bytearray
+    #        ('771333200013332000133320001333200013332000133320',
+    #         'utf-8')))
+    # print(Chart.make_out_string
+    #       (bytearray
+    #        ('000000000000000000000000000000000000000000000000',
+    #         'utf-8')))
 
 
 if __name__ == '__main__':
@@ -122,3 +157,19 @@ if __name__ == '__main__':
     # chart = Chart('/home/jazcap53/python_projects/spreadsheet_etl/src/chart/chart_raw_data.txt')
     # chart.open_file()
     main()
+
+
+# NOTES
+# >>> import datetime
+# >>> my_date = '2016-12-07'
+# >>> my_time = '23:45:00'
+# >>> my_datetime = my_date + ' ' + my_time
+# >>> my_datetime
+# '2016-12-07 23:45:00'
+# >>> my_date_and_time = my_date + ' ' + my_time
+# >>> my_datetime = datetime.datetime.strptime(my_date_and_time, '%Y-%m-%d %H:%M:%S')
+# >>> my_datetime
+# datetime.datetime(2016, 12, 7, 23, 45)
+# >>> my_newer_datetime = my_datetime + datetime.timedelta(minutes=15)
+# >>> my_newer_datetime
+# datetime.datetime(2016, 12, 8, 0, 0)
--- tests/test_chart.py
+++ tests/test_chart.py
@@ -49,10 +49,10 @@ def open_input_file(filename='/home/jazcap53/python_projects/' +
 
 
 def test_ctor_creates_class_vars(make_chart):
-    assert make_chart.to_glyph == {0: '\u2588', 1: '\u258c', 2: '\u2590',
-                                   3: '\u0020', 7: '\u2591'}
-    assert make_chart.ASLEEP == 0b0
-    assert make_chart.AWAKE == 0b1
+    # assert make_chart.to_glyph == {0: '\u2588', 1: '\u258c', 2: '\u2590',
+    #                                3: '\u0020', 7: '\u2591'}
+    assert make_chart.ASLEEP == 1
+    assert make_chart.AWAKE == 0
 
 
 def test_ctor_creates_instance_vars(make_chart):
@@ -60,88 +60,88 @@ def test_ctor_creates_instance_vars(make_chart):
     assert make_chart.cur_line is ''
 
 
-def test_quarter_to_wake_digit(make_chart):
+def test_quarter_to_int_asleep(make_chart):
     q = True
-    quarter = make_chart.quarter_to_digit(q)
-    assert quarter == make_chart.AWAKE
-
-
-def test_quarter_to_sleep_digit(make_chart):
-    q = False
-    quarter = make_chart.quarter_to_digit(q)
+    quarter = make_chart.quarter_to_int(q)
     assert quarter == make_chart.ASLEEP
 
 
-def test_quarters_to_glyph_code_00(make_chart):
-    hi = make_chart.ASLEEP
-    lo = make_chart.ASLEEP
-    code = make_chart.quarters_to_glyph_code(hi, lo)
-    assert code == 0
+def test_quarter_to_int_awake(make_chart):
+    q = False
+    quarter = make_chart.quarter_to_int(q)
+    assert quarter == make_chart.AWAKE
 
 
-def test_quarters_to_glyph_code_01(make_chart):
-    hi = make_chart.ASLEEP
-    lo = make_chart.AWAKE
-    code = make_chart.quarters_to_glyph_code(hi, lo)
-    assert code == 1
+# def test_quarters_to_glyph_code_00(make_chart):
+#     hi = make_chart.ASLEEP
+#     lo = make_chart.ASLEEP
+#     code = make_chart.quarters_to_glyph_code(hi, lo)
+#     assert code == 0
+#
+#
+# def test_quarters_to_glyph_code_01(make_chart):
+#     hi = make_chart.ASLEEP
+#     lo = make_chart.AWAKE
+#     code = make_chart.quarters_to_glyph_code(hi, lo)
+#     assert code == 1
+#
+#
+# def test_quarters_to_glyph_code_10(make_chart):
+#     hi = make_chart.AWAKE
+#     lo = make_chart.ASLEEP
+#     code = make_chart.quarters_to_glyph_code(hi, lo)
+#     assert code == 2
+#
+#
+# def test_quarters_to_glyph_code_11(make_chart):
+#     hi = make_chart.AWAKE
+#     lo = make_chart.AWAKE
+#     code = make_chart.quarters_to_glyph_code(hi, lo)
+#     assert code == 3
 
 
-def test_quarters_to_glyph_code_10(make_chart):
-    hi = make_chart.AWAKE
-    lo = make_chart.ASLEEP
-    code = make_chart.quarters_to_glyph_code(hi, lo)
-    assert code == 2
+# def test_make_glyph_if_0(make_chart):
+#     assert make_chart.make_glyph(0) == '\u2588'
+#
+#
+# def test_make_glyph_if_1(make_chart):
+#     assert make_chart.make_glyph(1) == '\u258c'
+#
+#
+# def test_make_glyph_if_2(make_chart):
+#     assert make_chart.make_glyph(2) == '\u2590'
+#
+#
+# def test_make_glyph_if_3(make_chart):
+#     assert make_chart.make_glyph(3) == '\u0020'
+#
+#
+# def test_make_glyph_if_7(make_chart):
+#     assert make_chart.make_glyph(7) == '\u2591'
 
 
-def test_quarters_to_glyph_code_11(make_chart):
-    hi = make_chart.AWAKE
-    lo = make_chart.AWAKE
-    code = make_chart.quarters_to_glyph_code(hi, lo)
-    assert code == 3
-
-
-def test_make_glyph_if_0(make_chart):
-    assert make_chart.make_glyph(0) == '\u2588'
-
-
-def test_make_glyph_if_1(make_chart):
-    assert make_chart.make_glyph(1) == '\u258c'
-
-
-def test_make_glyph_if_2(make_chart):
-    assert make_chart.make_glyph(2) == '\u2590'
-
-
-def test_make_glyph_if_3(make_chart):
-    assert make_chart.make_glyph(3) == '\u0020'
-
-
-def test_make_glyph_if_7(make_chart):
-    assert make_chart.make_glyph(7) == '\u2591'
-
-
-def test_make_out_string_with_valid_input(make_chart):
-    line_in = bytearray('771333200013332000133320001333200013332000133320',
-                        'utf-8')
-    assert len(line_in) == 48
-    temp2 = make_chart.make_out_string(line_in)
-    line_out = '|' + temp2 + '|\n'
-    assert len(line_out) == 51
-    assert line_out == '|░░▌   ▐███▌   ▐███▌   ▐███▌   ▐███▌   ▐███▌   ▐█|\n'
-
-
-def test_make_out_string_with_invalid_digit(make_chart):
-    with pytest.raises(KeyError):
-        # '8' in line_in
-        line_in = bytearray(
-            '771333200013332000133320801333200013332000133320', 'utf-8')
-        make_chart.make_out_string(line_in)
-
-
-def test_make_out_string_with_empty_string(make_chart):
-    with pytest.raises(AssertionError):
-        line_in = bytearray('', 'utf-8')
-        make_chart.make_out_string(line_in)
+# def test_make_out_string_with_valid_input(make_chart):
+#     line_in = bytearray('771333200013332000133320001333200013332000133320',
+#                         'utf-8')
+#     assert len(line_in) == 48
+#     temp2 = make_chart.make_out_string(line_in)
+#     line_out = '|' + temp2 + '|\n'
+#     assert len(line_out) == 51
+#     assert line_out == '|░░▌   ▐███▌   ▐███▌   ▐███▌   ▐███▌   ▐███▌   ▐█|\n'
+#
+#
+# def test_make_out_string_with_invalid_digit(make_chart):
+#     with pytest.raises(KeyError):
+#         # '8' in line_in
+#         line_in = bytearray(
+#             '771333200013332000133320801333200013332000133320', 'utf-8')
+#         make_chart.make_out_string(line_in)
+#
+#
+# def test_make_out_string_with_empty_string(make_chart):
+#     with pytest.raises(AssertionError):
+#         line_in = bytearray('', 'utf-8')
+#         make_chart.make_out_string(line_in)
 
 
 def test_get_a_line(open_input_file, make_chart):
