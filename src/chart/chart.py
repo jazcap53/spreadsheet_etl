cff47c3fb46c5f44a02f6435f1ecc80e0c114a84
Andrew Jarcho <andrew.jarcho@gmail.com>
2019-03-07 01:05:40 -0500

    Remove commented-out lines from previous version
    On branch to_landscape
    Changes to be committed:
        modified:   src/chart/chart.py

--- src/chart/chart.py
+++ src/chart/chart.py
@@ -13,15 +13,6 @@ class Chart:
     """
     Create a sleep chart from input data
     """
-    # to_glyph = {0: '\u2588',  # FULL BLOCK xx█xx
-    #             1: '\u258c',  # LEFT HALF BLOCK xx▌xx
-    #             2: '\u2590',  # RIGHT HALF BLOCK xx▐xx
-    #             3: '\u0020',  # SPACE xx xx
-    #             7: '\u2591'}  # LIGHT SHADE xx░xx
-
-    # ASLEEP = 0b0  # the printed color (black ink)
-    # AWAKE = 0b1  # the background color (white paper)
-
     ASLEEP = 1  # the printed color (black ink)
     AWAKE = 0   # the background color (white paper)
 
@@ -32,7 +23,6 @@ class Chart:
         self.prev_line = ''
         self.sleep_state = self.AWAKE
         self.date_re = None
-        # self.today = None
         self.cur_date_str = ''
         self.cur_time_str = ''
         self.cur_date_time_str = ''
@@ -71,38 +61,6 @@ class Chart:
             return Chart.ASLEEP
         return Chart.AWAKE
 
-    # @staticmethod
-    # def quarters_to_glyph_code(hi_bit, lo_bit):
-    #     """
-    #     Convert 2 1-bit quarters to a 2-bit code
-    #     :param hi_bit: high order bit
-    #     :param lo_bit: low order bit
-    #     :return: a 2-bit code representing a quarter hour
-    #     Called by:
-    #     """
-    #     return hi_bit << 1 | lo_bit
-
-    # @staticmethod
-    # def make_glyph(code):
-    #     """
-    #     A glyph names a Unicode Block Element code point
-    #     :param code: a 2-bit code representing a glyph in dict to_glyph
-    #     :return: the glyph associated with the 2-bit code
-    #     Called by: make_out_string()
-    #     """
-    #     return Chart.to_glyph[code]
-
-    # @staticmethod
-    # def make_out_string(line_in):
-    #     """
-    #     Convert bytearray with 2-bit values 0,1,2,3,or 7 to a string of glyphs
-    #     :param line_in:
-    #     :return:
-    #     Called by: main()
-    #     """
-    #     assert len(line_in)
-    #     return ''.join([Chart.make_glyph(int(i)) for i in line_in.decode()])
-
     def open_file(self):
         """
         :return: None
@@ -110,7 +68,6 @@ class Chart:
         """
         with open(self.filename) as self.infile:
             while self.get_a_line():
-                # print(self.cur_line)
                 print(self.parse_line(), self.cur_datetime)
 
     def parse_line(self):
@@ -122,12 +79,10 @@ class Chart:
         self.cur_time_str = line_array[1].strip()
         self.cur_date_time_str = self.cur_date_str + ((' ' + self.cur_time_str) if
                                                       self.cur_time_str else '')
-        # print(self.cur_date_time_str)
         self.cur_datetime = datetime.datetime.strptime(self.cur_date_time_str,
                                                        ('%Y-%m-%d %H:%M:%S' if
                                                         self.cur_time_str else
                                                         '%Y-%m-%d'))
-
         return line_array
 
     def compile_date_re(self):
@@ -142,20 +97,9 @@ def main():
     chart = Chart('/home/jazcap53/python_projects/spreadsheet_etl/src/chart/chart_raw_data.txt')
     chart.compile_date_re()
     chart.open_file()
-    # print(Chart.make_out_string
-    #       (bytearray
-    #        ('771333200013332000133320001333200013332000133320',
-    #         'utf-8')))
-    # print(Chart.make_out_string
-    #       (bytearray
-    #        ('000000000000000000000000000000000000000000000000',
-    #         'utf-8')))
 
 
 if __name__ == '__main__':
-    # chart = Chart(FileReadAccessWrapper('chart_raw_data.txt'))
-    # chart = Chart('/home/jazcap53/python_projects/spreadsheet_etl/src/chart/chart_raw_data.txt')
-    # chart.open_file()
     main()
 
 
