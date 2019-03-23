874e8d2bf9b1822a581b982956324b46a7a0d910
Andrew Jarcho <andrew.jarcho@gmail.com>
2019-03-23 18:45:03 -0400

    Add .sql file
    On branch mark_missing_data
    Changes to be committed:
        modified:   .gitignore
        new file:   sql_all/sql_queries_visualize/all_dates_all_cols_no_bedtime_with_no_data.sql

---
+++ sql_all/sql_queries_visualize/all_dates_all_cols_no_bedtime_with_no_data.sql
@@ -0,0 +1,22 @@
+-- get date, start_time, duration, nap_id for every date in range of data
+WITH each_date AS  -- get every date in range of data
+(SELECT i::date FROM generate_series((SELECT MIN(start_date) FROM sl_night), (SELECT MAX(start_date) FROM sl_night), '1 day'::interval) i)
+,
+everything AS  -- get all fields from both sl_night and sl_nap
+(SELECT sl_night.night_id AS ni_ni_id, start_date, sl_night.start_time AS ni_time,
+start_no_data, end_no_data,
+nap_id, sl_nap.start_time AS na_time, duration, sl_nap.night_id AS na_ni_id
+FROM sl_night JOIN sl_nap ON sl_night.night_id = sl_nap.night_id)
+-- put nulls into output when there is no data for a date
+SELECT each_date.i::date AS sleep_date, 
+    everything.na_time as nap_time, 
+    everything.duration AS duration,
+    everything.nap_id AS nap_id,
+    everything.start_no_data AS start_no_data,
+    everything.end_no_data AS end_no_data
+FROM each_date
+LEFT JOIN everything ON each_date.i::date = everything.start_date
+ORDER BY sleep_date, nap_id;
+
+
+-- note: if no bedtime happens on a calendar date, no data except for the date is output
