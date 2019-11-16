4ca5bce7a974c453cd22753ae8d1d0e9a98921c1
Andrew Jarcho <andrew.jarcho@gmail.com>
2019-11-15 20:17:10 -0500

    Remove commented-out lines from create_procedures_plpgsql.sql
    On branch refactor_05_chart_with_leading_NO_DATA
    Changes to be committed:
        modified:   db_s_etl/create_procedures_plpgsql.sql

--- db_s_etl/create_procedures_plpgsql.sql
+++ db_s_etl/create_procedures_plpgsql.sql
@@ -3,9 +3,6 @@
 -- 2017-04-05
 
 
--- the below are modeled on the last example at 
--- https://www.postgresql.org/docs/9.3/static/plpython-database.html
-
 CREATE OR REPLACE FUNCTION sl_insert_night(new_start_date date,
     new_start_time time without time zone,
     new_start_no_data boolean,
@@ -44,24 +41,3 @@ BEGIN
 
 END;
 $$ LANGUAGE plpgsql;
-
-
-
-
---
--- DROP FUNCTION sl_insert_nap(time without time zone, interval);
---
--- CREATE FUNCTION sl_insert_nap(new_start_time time without time zone,
---     new_duration interval hour to minute) RETURNS text AS $$
--- from plpy import spiexceptions
--- try:
---     rv = plpy.execute("SELECT currval('sl_night_night_id_seq') AS my_night_id")
---     plan = plpy.prepare("INSERT INTO sl_nap(start_time, duration, night_id) \
---             VALUES($1, $2, $3)", ["time without time zone",
---             "interval hour to minute", "integer"])
---     plpy.execute(plan, [new_start_time, new_duration, rv[0]["my_night_id"]])
--- except plpy.SPIError, e:
---     return "error: SQLSTATE %s" % (e.sqlstate,)
--- else:
---     return "sl_insert_nap() succeeded"
--- $$ LANGUAGE plpythonu;
