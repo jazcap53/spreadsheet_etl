6a30ff5e233159aa58e323c1be6b79f1e31510d6
Andrew Jarcho <andrew.jarcho@gmail.com>
2020-04-19 19:58:29 -0400

    Prevent sql procedures from inserting data already present in db
    On branch plpgsql
    Changes to be committed:
        modified:   db_s_etl/create_procedures_plpgsql.sql

--- db_s_etl/create_procedures_plpgsql.sql
+++ db_s_etl/create_procedures_plpgsql.sql
@@ -7,8 +7,18 @@ CREATE OR REPLACE FUNCTION sl_insert_night(new_start_date date,
     new_start_time time without time zone,
     new_start_no_data boolean,
     new_end_no_data boolean) RETURNS text AS $$
+DECLARE
+  sl_night_row sl_night%ROWTYPE;
 
 BEGIN
+    SELECT * INTO sl_night_row FROM sl_night WHERE start_date = new_start_date AND
+                                                   start_time = new_start_time AND
+                                                   start_no_data = new_start_no_data AND
+                                                   end_no_data = new_end_no_data;
+    IF FOUND THEN
+        RETURN 'sl_insert_night() failed: row already in table';
+    END IF;
+
     INSERT INTO sl_night (night_id, start_date, start_time, start_no_data, end_no_data)
     values (nextval('sl_night_night_id_seq'), new_start_date, new_start_time, new_start_no_data,
             new_end_no_data);
